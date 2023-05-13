21e3410667c7d58eba9678005487885a990dfdac
Victor kipchumba <victorkipchumba039@gmail.com>
2023-05-13 02:00:15 -0700

    For loop

---
+++ C/0x01-variables_if_else_while/4-for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 9 using for loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
