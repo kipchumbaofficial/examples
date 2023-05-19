7391bdbafe71fd2919e171416eb6181fc8075659
Victor kipchumba <victorkipchumba039@gmail.com>
2023-05-19 01:39:43 -0700

    Size of Basic Data Types in C

---
+++ C/0x04-pointers_arrays/0-sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints size of for basic data types in C
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of 'char' in my computer is: %lu\n", sizeof(char));
+	printf("Size of 'int' in my computer is: %lu\n", sizeof(int));
+	printf("Size of 'float' in my computer is: %lu\n", sizeof(float));
+	printf("Size of 'double' in my computer is: %lu\n", sizeof(double));
+	return (0);
+}
