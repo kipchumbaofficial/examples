acc0c6394f0792091bf746c8cc136905599d3a00
Victor kipchumba <victorkipchumba039@gmail.com>
2023-05-23 00:27:51 -0700

    NULL Pointer

---
+++ C/0x04-pointers_arrays/19-null_pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A null pointer illustation
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int *ptr = NULL;
+
+	printf("Value of ptr is: %p\n", ptr);
+	return (0);
+}
