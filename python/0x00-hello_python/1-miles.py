b98302cb7b09858eee1d13ec6fcf7295704f2766
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-13 22:23:35 -0700

    Convert kilometers to miles

---
+++ python/0x00-hello_python/1-miles.py
@@ -0,0 +1,12 @@
+# Receive miles and convert to kilometers
+
+miles = input('Enter miles: ')
+
+# convert to int
+miles = int(miles)
+
+# conver to kilometers
+kilometers = miles * 1.60934
+
+# print kilometers
+print(f"{miles} miles equals {kilometers} kilometers")
