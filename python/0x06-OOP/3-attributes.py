2f05ecc9350fed4a206b8130f6c0c0235027afcc
Victor Kipchumba <victorkipchumba039@gmai.com>
2023-09-04 20:08:13 -0700

    Simple class fields

---
+++ python/0x06-OOP/3-attributes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Robot:
+    pass
+x = Robot()
+y = Robot()
+x.name = 'Marvin'
+x.build_year = '1979'
+y.name = 'Caliban'
+y.build_year = '1993'
+
+print(x.name)
+print(y.name)
