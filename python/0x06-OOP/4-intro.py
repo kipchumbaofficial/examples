6048cf0d5a9bca319243d5678691d1e8c33b911c
victor kipchumba <victorkipchumba039@gmail.com>
2024-01-02 20:40:22 +0300

    trial

---
+++ python/0x06-OOP/4-intro.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class dog
+"""
+
+
+class Dog:
+    """Defines dog
+    """
+    def __init__(self):
+        pass
+
+max = Dog()
+print(max)
---
+++ python/0x06-OOP/5-freeCodeCamp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Item:
+    pass
+
+item1 = Item()
+item1.name = 'phone'
+item1.price = 100
+item1.quantity = 5
+
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
