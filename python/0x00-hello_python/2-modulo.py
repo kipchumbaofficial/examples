98d335e791206870d17758574f96e01c77378e98
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-26 15:47:36 +0300

    Using dictionary and tuple as RH operand

--- python/0x00-hello_python/2-modulo.py
+++ python/0x00-hello_python/2-modulo.py
@@ -2,5 +2,7 @@
 # Example using modulo for string interpolation
 
 name = 'World'
+learner = 'Kipchumba'
 
-print("Hello %s!" % name)
+print("Hello %s!, I am %s" % (name, learner))
+print("Hello %(name)s!, You are %(age)s years old" % {"name": "Victor", "age": 23})
