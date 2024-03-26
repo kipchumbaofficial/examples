d53caa0298a9a7b8db51f944cb0d68e27d35cf9c
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-26 16:23:35 +0300

    Wrap up of .format() method

--- python/0x00-hello_python/3-format.py
+++ python/0x00-hello_python/3-format.py
@@ -5,4 +5,15 @@
 name = "Victor"
 age = "23"
 
+person = {"name": "Chirchir", "age": 23}
+
 print("Hello I am {}, I am {}".format(name, age))
+# Using Zero based index to specify position
+
+print("I am {1}, My name is {0}".format(name, age))
+
+# Using keyworded argument
+print("Hello I am {name}, I am {age}".format(name="Kipchumba", age=23))
+
+# Using dictionary and ** unpacking operator
+print("Hello I am {name}, I am {age}".format(**person))
