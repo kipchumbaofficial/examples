1fc9bc0f9b8ceabaff8e307aa505a15338367198
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-03-26 21:43:04 +0300

    Running pyton scripts

--- python/0x00-hello_python/4-fstrings.py
+++ python/0x00-hello_python/4-fstrings.py
@@ -6,3 +6,11 @@ name = "Victor"
 age = 23
 
 print(f"Hello I am {name}, I am {age}")
+
+# Embeding expressions into f -strings
+print(f"The sum of 400067 and 367000 is {400067 + 367000}")
+# Function calls
+print(f"Hello I om {name.upper()}, I am {age}")
+
+# Comphension
+print(f"{[2**n for n in range(0, 10)]}")
