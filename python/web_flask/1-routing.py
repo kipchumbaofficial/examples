2ca9b73fc03195f07765b6b4f257ea7cca8338cb
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-18 22:26:32 +0300

    Task 1

---
+++ python/web_flask/1-routing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Index page"
+@app.route('/hello')
+def hello():
+    return "Hello Flask"
