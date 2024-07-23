709ce2d4e325a24b0b3e28f4372e7b2ab9297774
Kipchumba Victor <victorkipchumba039@gmail.com>
2024-07-23 10:09:12 +0300

    Array destructuring

---
+++ javascript/es6/4-array_destructuring.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// Array Destructuring
+
+const fruits = ['Bananas', 'Oranges', 'Grapes', 'Mango']
+
+//Destructuring
+
+let [fruit1, fruit2] = fruits;
+
+console.log(fruit1, fruit2)
